7471b242d08ec372bf65fd7ea6a699e4c3790a9d
vampytam <3013135559@qq.com>
2025-01-18 17:18:50 +0800

    can finetune

---
+++ finetune_beit.sh
@@ -0,0 +1,23 @@
+export PYTHONPATH=$PYTHONPATH:$(pwd)
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+export OMP_NUM_THREADS=1
+
+python -m torch.distributed.launch --nproc_per_node=4 --master_port=29502 runs/run_class_finetuning.py \
+    --model "beit_base_patch16_224" `# 要微调的模型名称` \
+    --data_path "data/imagewoof2-320/val" `# 数据集路径` \
+    --nb_classes 10 `# 分类类别数量` \
+    --data_set "image_folder" `# 数据集类型` \
+    --disable_eval_during_finetuning `# 在微调过程中禁用评估` \
+    --finetune "output/pretrain_output/checkpoint-19.pth" `# 预训练模型权重路径` \
+    --output_dir "output/finetune_output" `# 保存模型的输出路径` \
+    --save_ckpt_freq 5 `# 保存模型的频率` \
+    --batch_size 16 `# 每个批次的样本数量` \
+    --epochs 20 `# 训练的总轮数` \
+    --lr 3e-3 `# 学习率` \
+    --update_freq 1 `# 参数更新频率` \
+    --warmup_epochs 5 `# 预热学习率的轮数` \
+    --layer_decay 0.65 `# 层衰减率，控制不同层的学习率` \
+    --drop_path 0.2 `# 路径丢弃率(Drop Path)` \
+    --weight_decay 0.05 `# 权重衰减率，用于正则化` \
+    --layer_scale_init_value 0.1 `# 层缩放初始化值` \
+    --clip_grad 3.0 `# 梯度裁剪阈值，防止梯度爆炸`
--- pretrain_beit.sh
+++ pretrain_beit.sh
@@ -1,13 +1,14 @@
+export PYTHONPATH=$PYTHONPATH:$(pwd)
 OMP_NUM_THREADS=1
 
 python -m torch.distributed.launch --nproc_per_node=4 runs/run_beit_pretraining.py \
-    --data_path "data/imagewoof/train" `# 预训练数据集路径` \
-    --output_dir "data/output/" `# 保存模型的路径` \
+    --data_path "data/imagewoof2-320/train" `# 预训练数据集路径` \
+    --output_dir "output/pretrain_output/" `# 保存模型、日志的路径` \
     --model "beit_base_patch16_224_8k_vocab" `# 要训练的模型名称` \
     --discrete_vae_type "dall-e" `# VAE的类型` \
     --discrete_vae_weight_path "data/dalle" `# VAE权重的路径` \
     --num_mask_patches 75 `# 需要掩蔽的视觉标记/补丁数量` \
-    --batch_size 128 `# 每个批次的样本数量` \
+    --batch_size 16 `# 每个批次的样本数量` \
     --lr 5e-4 `# 学习率` \
     --warmup_epochs 5 `# 预热学习率的轮数` \
     --epochs 20 `# 训练的总轮数` \
